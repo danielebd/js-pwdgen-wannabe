a5f408df590992a7ddfe64003766fc1cf00798dc
danielebd <danielebalebalestro@gmail.com>
2023-02-20 16:26:26 +0100

    prompt cognome

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //restrizioni per l'apprensimento
 'use strict';
 
-//promp per Nome
+//prompt per Nome
 const firstName = prompt('inserisci il tuo nome');
 
 
+//prompt per Cognome
+const secondName = prompt('inserisci il tuo cognome');
 
